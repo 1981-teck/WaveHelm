6c7d6f70921132d9309a05f5efb612525f6f7eaa
Pastoris Marco Vincenzo <mvsoundsystem81@gmail.com>
2026-08-19 18:25:11 +0200

    Remove obsolete divergent SQL artifacts

--- src/sql/playlist.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS playlists (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE,
-    description TEXT,
-    cover_art TEXT,
-    creation_date TEXT NOT NULL,
-    last_modified TEXT NOT NULL
-);
